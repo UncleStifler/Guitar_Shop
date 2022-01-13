a5376222d6e33c99a9d32a8071a8fd1833a747c3
UncleStifler <ambalexey@gmail.com>
2022-01-13 11:16:58 +0100

    the logic of working with goods, changing the button style, and prescribed the logic of adding an item to cart through the localStorage

---
+++ constants/catalog.js
@@ -0,0 +1,26 @@
+const CATALOG = [
+    {
+        id: "el1",
+        name: "Harley Benton R-457MN WH Fanfret",
+        img: "img/catalog/427245.jpg",
+        price: 170,
+    },
+    {
+        id: "el2",
+        name: "Music Man John Petrucci Majesty 7 HS Ltd",
+        img: "img/catalog/520931.jpg",
+        price: 6699,
+    },
+    {
+        id: "el3",
+        name: "Harley Benton R-457BK Fanfret",
+        img: "img/catalog/410817.jpg",
+        price: 179,
+    },
+    {
+        id: "el4",
+        name: "Harley Benton R-457MN WH Progressive Series",
+        img: "img/catalog/419527.jpg",
+        price: "149",
+    },
+]
